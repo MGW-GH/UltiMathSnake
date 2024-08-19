8e49deb1381294753162deb7059393ebd32174a4
Matthew Webster <contactmattwebster@gmail.com>
2024-08-19 13:32:56 +0000

    added font awesome icon to header and styled in css looking at mobile first

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
 // board
-const cellSize = 30;
-const rows = 22;
+const cellSize = 14;
+const rows = 32;
 const columns = 22;
 let context;
 let board;
 
 // snake
-let snakeX = cellSize * Math.floor(Math.random()*rows);
-let snakeY = cellSize * Math.floor(Math.random()*columns);
+let snakeX = cellSize * Math.floor(Math.random()*columns);
+let snakeY = cellSize * Math.floor(Math.random()*rows);
 
 // food
-let foodX = cellSize * Math.floor(Math.random()*rows);
-let foodY = cellSize * Math.floor(Math.random()*columns);
+let foodX = cellSize * Math.floor(Math.random()*columns);
+let foodY = cellSize * Math.floor(Math.random()*rows);
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -30,7 +30,7 @@ function update() {
 
     context.fillStyle = "Lime";
     context.beginPath();
-    context.arc(snakeX + cellSize / 2, snakeY + cellSize / 2, cellSize / 2, 0, Math.PI * 2);
+    context.arc(snakeX + cellSize / 2, snakeY + cellSize / 2, cellSize / 1.5, 0, Math.PI * 2);
     context.fill();
     
     context.fillStyle = "red";
@@ -41,7 +41,7 @@ function update() {
 
 function placeFood() {
     do {
-        foodX = cellSize * Math.floor(Math.random() * rows);
-        foodY = cellSize * Math.floor(Math.random() * columns);
+        foodX = cellSize * Math.floor(Math.random() * columns);
+        foodY = cellSize * Math.floor(Math.random() * rows);
     } while (foodX === snakeX && foodY === snakeY);  // Ensure food doesn't overlap with the snake
 }
