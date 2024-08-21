6dde8ba0d75821295372ec11a95f73fc0cd4f322
Matthew Webster <contactmattwebster@gmail.com>
2024-08-21 10:58:36 +0000

    finished adding touch key functionality in js

--- assets/script.js
+++ assets/script.js
@@ -37,7 +37,21 @@ window.onload = function() {
     context = board.getContext("2d");
 
     placeNumbers();
+    // Add event listeners for arrow keys
     document.addEventListener("keyup", changeDirection);
+    // Add event listeners for touch controls
+    document.getElementById('up-btn').addEventListener('click', function(e) {
+        changeDirection({ target: e.target });
+    });
+    document.getElementById('down-btn').addEventListener('click', function(e) {
+        changeDirection({ target: e.target });
+    });
+    document.getElementById('left-btn').addEventListener('click', function(e) {
+        changeDirection({ target: e.target });
+    });
+    document.getElementById('right-btn').addEventListener('click', function(e) {
+        changeDirection({ target: e.target });
+    });
     setInterval(update, 1000/10);
     
 
@@ -132,22 +146,23 @@ function placeNumbers() {
 }
 
 function changeDirection(e) {
-    if (e.code == "ArrowUp" && velocityY != 1) {
+
+    if ((e.code == "ArrowUp" || e.target.id === "up-btn") && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
     }
 
-    else if (e.code == "ArrowDown" && velocityY != -1) {
+    else if ((e.code == "ArrowDown" || e.target.id === "down-btn") && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
     }
 
-    else if (e.code == "ArrowLeft" && velocityX != 1) {
+    else if ((e.code == "ArrowLeft" || e.target.id === "left-btn") && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
     }
 
-    else if (e.code == "ArrowRight" && velocityX != -1) {
+    else if ((e.code == "ArrowRight" || e.target.id === "right-btn") && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
